933e649f0d7e53a97e002a9757764d061e4a6ec2
Ebubekir Guldal <ebubekir57450@gmail.com>
2026-01-28 10:05:18 +0100

    Finalisation de la map

--- RogueLike/Domain/Catalogs/MapCatalog.cs
+++ RogueLike/Domain/Catalogs/MapCatalog.cs
@@ -7,11 +7,25 @@ public static class MapCatalog
 {
     public static GameMap Level1()
     {
-        var map = new MapBuilder(30, 15)
+        var map = new MapBuilder(30, 18)
             .DrawBorder(TileType.Wall)
-            .DrawRect(1, 4, 5, 1, TileType.Wall)
+            .DrawRect(1, 4, 6, 1, TileType.Wall)
+
+            .DrawRect(13, 1, 1, 4, TileType.Wall)
+
+            .DrawRect(11, 5, 6, 1, TileType.Wall)
+            .DrawRect(11, 6, 1, 2, TileType.Wall)
+            .DrawRect(11, 9, 1, 2, TileType.Wall)
+            .DrawRect(11, 11, 6, 1, TileType.Wall)
+            .DrawRect(16, 5, 1, 6, TileType.Wall)
+
+            .DrawRect(13, 12, 1, 2, TileType.Wall)
+            .DrawRect(14, 13, 7, 1, TileType.Wall)
+            .DrawRect(14, 15, 7, 1, TileType.Wall)
+            .DrawRect(13, 15, 1, 2, TileType.Wall)
+            
             .DrawRect(1, 10, 5, 1, TileType.Wall)
-            .DrawRect(5, 10, 1, 4, TileType.Wall)
+            .DrawRect(5, 10, 1, 7, TileType.Wall)
             // .SetTile(18, 1, TileType.Exit)
             // .SetTile(10, 3, TileType.DoorClosed)
             .Build();
